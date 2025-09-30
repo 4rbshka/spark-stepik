1a413c05a0043e482c102dbccd8e0b4364d91bcb
Mikhail Golubev <MGolubev@interprocom.ru>
2025-09-30 15:15:48 +0300

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,14 @@
+name := "spark-stepik"
+
+version := "0.1"
+
+scalaVersion := "2.13.8"
+
+
+lazy val sparkVersion = "3.2.1"
+
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % sparkVersion,
+  "org.apache.spark" %% "spark-sql" % sparkVersion
+)
---
+++ src/main/scala/Playground.scala
@@ -0,0 +1,40 @@
+import org.apache.spark.sql.SparkSession
+
+import org.apache.log4j.{Level, Logger}
+
+object Playground extends App {
+  Logger.getLogger("org").setLevel(Level.ERROR)
+  Logger.getLogger("akka").setLevel(Level.ERROR)
+
+  val spark = SparkSession.builder()
+    .appName("Playground")
+    .master("local")
+    .getOrCreate()
+
+  import org.apache.spark.sql.Row
+
+  val data = Seq(
+    Row("s9FH4rDMvds", "2020-08-11T22:21:49Z", "UCGfBwrCoi9ZJjKiUK8MmJNw", "2020-08-12T00:00:00Z"),
+    Row("kZxn-0uoqV8", "2020-08-11T14:00:21Z", "UCGFNp4Pialo9wjT9Bo8wECA", "2020-08-12T00:00:00Z"),
+    Row("QHpU9xLX3nU", "2020-08-10T16:32:12Z", "UCAuvouPCYSOufWtv8qbe6wA", "2020-08-12T00:00:00Z")
+  )
+
+  import org.apache.spark.sql.types._
+
+  val schema = Array(
+    StructField("videoId", StringType, false),
+    StructField("publishedAt", StringType, false),
+    StructField("channelId", StringType, false),
+    StructField("trendingDate", StringType, false)
+  )
+
+
+  val df = spark.createDataFrame(
+    spark.sparkContext.parallelize(data),
+    StructType(schema)
+  )
+
+  df.show()
+  df.printSchema()
+
+}
