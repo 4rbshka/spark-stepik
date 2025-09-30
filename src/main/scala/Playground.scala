cc8751c6602b63e0a730bb09b55bdb0c24cfef5f
Mikhail Golubev <MGolubev@interprocom.ru>
2025-09-30 18:28:58 +0300

    fix style

--- src/main/scala/Playground.scala
+++ src/main/scala/Playground.scala
@@ -1,6 +1,7 @@
 import org.apache.spark.sql.SparkSession
-
 import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.types._
 
 object Playground extends App {
   Logger.getLogger("org").setLevel(Level.ERROR)
@@ -11,15 +12,25 @@ object Playground extends App {
     .master("local")
     .getOrCreate()
 
-  import org.apache.spark.sql.Row
+  val sc = spark.sparkContext
 
   val data = Seq(
-    Row("s9FH4rDMvds", "2020-08-11T22:21:49Z", "UCGfBwrCoi9ZJjKiUK8MmJNw", "2020-08-12T00:00:00Z"),
-    Row("kZxn-0uoqV8", "2020-08-11T14:00:21Z", "UCGFNp4Pialo9wjT9Bo8wECA", "2020-08-12T00:00:00Z"),
-    Row("QHpU9xLX3nU", "2020-08-10T16:32:12Z", "UCAuvouPCYSOufWtv8qbe6wA", "2020-08-12T00:00:00Z")
+    Row(
+      "s9FH4rDMvds",
+      "2020-08-11T22:21:49Z",
+      "UCGfBwrCoi9ZJjKiUK8MmJNw",
+      "2020-08-12T00:00:00Z"),
+    Row(
+      "kZxn-0uoqV8",
+      "2020-08-11T14:00:21Z",
+      "UCGFNp4Pialo9wjT9Bo8wECA",
+      "2020-08-12T00:00:00Z"),
+    Row(
+      "QHpU9xLX3nU",
+      "2020-08-10T16:32:12Z",
+      "UCAuvouPCYSOufWtv8qbe6wA",
+      "2020-08-12T00:00:00Z")
   )
-
-  import org.apache.spark.sql.types._
 
   val schema = Array(
     StructField("videoId", StringType, false),
@@ -30,7 +41,7 @@ object Playground extends App {
 
 
   val df = spark.createDataFrame(
-    spark.sparkContext.parallelize(data),
+    sc.parallelize(data),
     StructType(schema)
   )
 
