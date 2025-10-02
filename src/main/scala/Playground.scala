df32e5d6fd970e9253eb3e2b2d59e6df959dae02
Mikhail Golubev <MGolubev@interprocom.ru>
2025-10-02 11:37:28 +0300

    select

--- src/main/scala/Playground.scala
+++ src/main/scala/Playground.scala
@@ -1,6 +1,4 @@
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.sql.SaveMode
-import org.apache.spark.sql.types._
 
 object Playground extends App with Context {
   Logger.getLogger("org").setLevel(Level.ERROR)
@@ -8,39 +6,14 @@ object Playground extends App with Context {
 
   override val appName: String = "Playground"
 
-  val moviesSchema = StructType(Seq(
-    StructField("index", IntegerType),
-    StructField("show_id", StringType),
-    StructField("type", StringType),
-    StructField("title", StringType),
-    StructField("director", StringType),
-    StructField("cast", StringType),
-    StructField("country", StringType),
-    StructField("date_added", StringType),
-    StructField("release_year", IntegerType),
-    StructField("rating", StringType),
-    StructField("duration", StringType),
-    StructField("listed_in", StringType),
-    StructField("description", StringType),
-    StructField("year_added", IntegerType),
-    StructField("month_added", FloatType),
-    StructField("season_count", IntegerType)
-  ))
-
-  val moviesDF = spark.read
+  val bikeSharingDF = spark.read
     .format("csv")
-    .schema(moviesSchema)
+    .option("inferSchema", "true")
     .option("header", "true")
-    .option("mode", "failFast")
-    .option("nullValue", "n/a")
-    .option("escape", "\"")
-    .option("quote", "\"")
-    .option("multiLine", "true")
-    .load("src/main/resources/movies_on_netflix.csv")
+    .load("src/main/resources/bike_sharing.csv")
+    .select("Hour",
+      "TEMPERATURE",
+      "HUMIDITY", "WIND_SPEED")
 
-  moviesDF.printSchema()
-
-  moviesDF.write
-    .mode(SaveMode.Overwrite)
-    .save("src/main/resources/data/movies_on_netflix.parquet")
+  bikeSharingDF.show(3)
 }
