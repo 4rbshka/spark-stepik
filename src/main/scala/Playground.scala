a2ce95bb9769eede3ce83d4d8d46a2d8057fdcfe
Mikhail Golubev <MGolubev@interprocom.ru>
2025-10-19 19:31:02 +0300

    find Positions

--- src/main/scala/Playground.scala
+++ src/main/scala/Playground.scala
@@ -1,114 +1,39 @@
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
-import org.apache.spark.sql.functions._
+import org.apache.spark.sql.Dataset
 
-object Playground extends App {
+object Playground extends App with Context {
   Logger.getLogger("org").setLevel(Level.ERROR)
   Logger.getLogger("akka").setLevel(Level.ERROR)
 
-  private def createSession(appName: String): SparkSession = {
-    SparkSession.builder()
-      .appName(appName)
-      .master("local[*]")
-      .getOrCreate()
-  }
-
-  val appName: String = "Playground"
-  lazy val spark = createSession(appName)
-
+  override val appName: String = "Playground"
   import spark.implicits._
 
-  case class Cars(
-                   id: Int,
-                   price: Int,
-                   brand: String,
-                   type_car: String,
-                   mileage: Double,
-                   color: String,
-                   date_of_purchase: String
-                 )
+  case class Employee(
+                       positionID: Int,
+                       position: String
+                     )
 
-  case class CarsAge(
-                      id: Int,
-                      price: Int,
-                      brand: String,
-                      type_car: String,
-                      mileage: Double,
-                      color: String,
-                      date_of_purchase: String,
-                      avg_mileage: Double,
-                      years_since_purchase: Int
-                    )
+  val hrDataset: Dataset[Employee] = spark.read
+    .option("header", "true")
+    .option("inferSchema", "true")
+    .csv("src/main/resources/hrdataset.csv")
+    .as[Employee]
+    .map(employee => Employee(employee.positionID, employee.position))
 
-  def readCarsData(filePath: String): DataFrame = {
-    spark.read
-      .option("inferSchema", "true")
-      .option("header", "true")
-      .csv(filePath)
+  def extractFirstWord(position: String): String =
+    position.split(" ").headOption.getOrElse("").toLowerCase
+
+  def findPositionsByPartialNames(partialNames: List[String]): Dataset[Employee] = {
+    val searchWords = partialNames.map(_.toLowerCase).toSet
+
+    hrDataset
+      .filter(employee => searchWords(extractFirstWord(employee.position)))
+      .distinct()
+      .orderBy("positionID")
   }
 
-  def normalizeColumnNames: DataFrame => DataFrame = { df =>
-    df.withColumnRenamed("type", "type_car")
-  }
+  val query = List("BI", "it")
+  val resultDS = findPositionsByPartialNames(query)
 
-  def unifyDateFormats: DataFrame => DataFrame = { df =>
-    df.withColumn("date_of_purchase",
-      date_format(
-        coalesce(
-          to_date(col("date_of_purchase"), "yyyy MM dd"),
-          to_date(col("date_of_purchase"), "yyyy-MM-dd"),
-          to_date(col("date_of_purchase"), "yyyy MMM dd"),
-          to_date(col("date_of_purchase"), "MM/dd/yyyy")
-        ),
-        "yyyy-MM-dd"
-      ))
-  }
-
-  def fillMissingMileage: DataFrame => DataFrame = { df =>
-    df.na.fill(0.0, List("mileage"))
-  }
-
-  def calculateAverageMileage: Dataset[Cars] => Double = { ds =>
-    ds.map(_.mileage).reduce(_ + _) / ds.count()
-  }
-
-  def calculateCarAge(dateOfPurchase: String, currentDate: String = "2025-10-16"): Int = {
-    val sdf = new java.text.SimpleDateFormat("yyyy-MM-dd")
-    val currentMillis = sdf.parse(currentDate).getTime
-    val purchaseMillis = sdf.parse(dateOfPurchase).getTime
-
-    val daysSincePurchase = (currentMillis - purchaseMillis) / (1000 * 60 * 60 * 24)
-    (daysSincePurchase / 365.25).toInt
-  }
-
-  def addCarAgeInfo(avgMileage: Double): Dataset[Cars] => Dataset[CarsAge] = { ds =>
-    ds.map { car =>
-      val yearsSincePurchase = calculateCarAge(car.date_of_purchase)
-
-      CarsAge(
-        car.id,
-        car.price,
-        car.brand,
-        car.type_car,
-        car.mileage,
-        car.color,
-        car.date_of_purchase,
-        avgMileage,
-        yearsSincePurchase
-      )
-    }
-  }
-
-  val carsPipeline: Dataset[CarsAge] = readCarsData("src/main/resources/cars.csv")
-    .transform(normalizeColumnNames)
-    .transform(unifyDateFormats)
-    .transform(fillMissingMileage)
-    .as[Cars]
-    .transform { ds =>
-      val avgMileage = calculateAverageMileage(ds)
-      println(s"Average mileage: $avgMileage")
-      addCarAgeInfo(avgMileage)(ds)
-    }
-
-  carsPipeline.show()
+  resultDS.show(false)
 }
